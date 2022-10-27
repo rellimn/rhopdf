a7938538844e1a1f26d0ec7391d35ab0a0ce34d1
roman <r>
2022-10-27 17:42:08 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,33 @@
+# This is a very simple Makefile that calls 'gradlew' to do the heavy lifting.
+
+default: debug
+
+debug:
+	./gradlew --warning-mode=all assembleDebug bundleDebug
+release:
+	./gradlew --warning-mode=all assembleRelease bundleRelease
+install:
+	./gradlew --warning-mode=all installDebug
+install-release:
+	./gradlew --warning-mode=all installRelease
+lint:
+	./gradlew --warning-mode=all lint
+archive:
+	./gradlew --warning-mode=all publishReleasePublicationToLocalRepository
+sync: archive
+	rsync -av --chmod=g+w --chown=:gs-priv $(HOME)/MAVEN/com/ ghostscript.com:/var/www/maven.ghostscript.com/com/
+
+run: install
+	adb shell am start -n com.artifex.mupdf.viewer.app/.LibraryActivity
+run-release: install-release
+	adb shell am start -n com.artifex.mupdf.viewer.app/.LibraryActivity
+
+tarball: release
+	cp app/build/outputs/apk/release/app-universal-release.apk \
+		mupdf-$(shell git describe --tags)-android-viewer.apk
+
+clean:
+	rm -rf .gradle build
+	rm -rf jni/.cxx jni/.externalNativeBuild jni/.gradle jni/build
+	rm -rf lib/.gradle lib/build
+	rm -rf app/.gradle app/build
---
+++ app/src/main/java/de/rw/rhopdf/LaunchActivity.java
@@ -0,0 +1,47 @@
+package de.rw.rhopdf;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.artifex.mupdf.viewer.DocumentActivity;
+
+public class LaunchActivity extends Activity
+{
+	protected final int FILE_REQUEST = 42;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	public void onStart() {
+		super.onStart();
+		ComponentName componentToDisable =
+				new ComponentName("de.rw.rhopdf",
+						"de.rw.rhopdf.LaunchActivity");
+
+		getPackageManager().setComponentEnabledSetting(
+				componentToDisable,
+				PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+				PackageManager.DONT_KILL_APP);
+	}
+
+	public void onActivityResult(int request, int result, Intent data) {
+		if (request == FILE_REQUEST && result == Activity.RESULT_OK) {
+			if (data != null) {
+				Intent intent = new Intent(this, DocumentActivity.class);
+				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+				intent.addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+				intent.setAction(Intent.ACTION_VIEW);
+				intent.setDataAndType(data.getData(), data.getType());
+				intent.putExtra(getComponentName().getPackageName() + ".ReturnToLibraryActivity", 1);
+				startActivity(intent);
+			}
+			finish();
+		} else if (request == FILE_REQUEST && result == Activity.RESULT_CANCELED) {
+			finish();
+		}
+	}
+}
